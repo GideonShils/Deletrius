4fa9cd74fdb3270b2f234c147ca9af973d3cb458
Gideon Shils <gideon.shils@gmail.com>
2018-05-29 14:35:08 -0400

    start twitter auth

--- client/src/App.js
+++ client/src/App.js
@@ -8,11 +8,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Twitter Delete</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <a href="/api/auth/twitter" className="App-intro">
+          Login with twitter
+        </a>
       </div>
     );
   }
--- server.js
+++ server.js
@@ -2,9 +2,11 @@ import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import passport from 'passport';
+var authStrategy = require('passport-twitter').Strategy;
 require('dotenv').config();
 
-// Setup server
+// Setup server & routes
 const app = express();
 
 app.set('port', process.env.PORT || 3001);
@@ -13,21 +15,42 @@ app.use(bodyParser.json());
 app.use(logger('dev'));
 
 const router = express.Router();
+
 router.get('/', (req, res) => {
 	res.send({message: 'Hello World'});
 })
 
+router.get('/auth/twitter', passport.authenticate('twitter'));
+
+router.get('/auth/twitter/callback',
+	passport.authenticate('twitter'),
+	function(req, res) {
+		console.log(res);
+	});
+
 app.use('/api', router);
+
+
+// Twitter authentication
+passport.use(new authStrategy ({
+	consumerKey: process.env.CONSUMER_KEY,
+	consumerSecret: process.env.CONSUMER_SECRET,
+	callbackURL: "http://127.0.0.1:3001/auth/twitter/callback"
+	},
+	function(token, tokenSecret, profile, callback) {
+		return callback(null, profile)
+	}
+));
+
+const Schema = mongoose.Schema;
+
+// Create schema for storing tweets
+const timelineSchema = new Schema({
+	author: String,
+	content: String,
+	id: Number,
+})
 
 app.listen(app.get('port'), () => {
 	console.log('Express server listening on port ' + app.get('port'));
 });
-
-
-// Twitter stuff
-var twit = {
-	consumer_key: process.env.TWIT_CONS_KEY ,
-	consumer_secret: process.env.TWIT_CONS_SEC,
-	access_token: process.env.TWIT_ACC_TOK,
-	access_token_secret: process.env.TWIT_ACC_SEC
-};
