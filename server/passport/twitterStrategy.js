f2b1bf4ec8c9fdd120317db7efb3a06f11bc91b3
Gideon Shils <gideon.shils@gmail.com>
2018-09-24 19:48:10 -0400

    Update callback url

--- server/passport/twitterStrategy.js
+++ server/passport/twitterStrategy.js
@@ -7,7 +7,7 @@ const strategy = new TwitterStrategy (
 	{
 	consumerKey: process.env.CONSUMER_KEY,
 	consumerSecret: process.env.CONSUMER_SECRET,
-	callbackURL: "http://127.0.0.1:3001/auth/twitter/callback"
+	callbackURL: process.env.CALLBACK_URL
 	},
 	function(token, tokenSecret, profile, callback) {
 		const query = { userId: profile.id };
